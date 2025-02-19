f056e97ba246741d3fd38b4405757c3b8a7cc442
creative-ps <crativity.im@gmail.com>
2025-02-19 21:06:11 +0530

    product page

--- src/views/ProductPage.js
+++ src/views/ProductPage.js
@@ -1,8 +1,9 @@
 import React from "react";
+import { useParams } from "react-router";
 export default function ProductPage(){
     return <>
                 <div className="productPage">
-                    
+
                 </div>
            </>;
 }
