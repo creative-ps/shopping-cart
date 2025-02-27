61cbc22c926a2e9662fd84e81d3c613467cfd379
creative-ps <crativity.im@gmail.com>
2025-02-27 09:47:36 +0530

    indexJs Modifies

--- index.js
+++ index.js
@@ -1,11 +1,12 @@
 import React from "react";
 import { createRoot } from "react-dom/client";
-import { BrowserRouter, Routes, Route } from "react-router";
+import { BrowserRouter, Routes, Route, useParams } from "react-router";
 import App from "./src/App";
 import SearchResult from "./src/SearchResult";
 import Main from "./src/Main";
 import Cart from "./src/Cart";
 import ShippingAddress from "./src/ShippingAddress";
+import ProductPage from "./src/views/ProductPage";
 
 
 const root = createRoot(document.getElementById("root"));
@@ -18,6 +19,7 @@ root.render(
                             <Route path="/category/:cate" element={<SearchResult/>}></Route>
                             <Route path="/cart" element={<Cart/>}></Route>
                             <Route path="/shippingAddress" element={<ShippingAddress/>}></Route>
+                            <Route path="/:category/:title" element={<ProductPage/>}></Route>
                         </Route>
                     </Routes>
                 </BrowserRouter>
--- src/views/ProductPage.js
+++ src/views/ProductPage.js
@@ -3,7 +3,7 @@ import { useParams } from "react-router";
 export default function ProductPage(){
     return <>
                 <div className="productPage">
-
+                    <h2>This is Product page.</h2>
                 </div>
            </>;
 }
